6808205d18d2435d39abbf07a8d8d2144524bf1e
Joseph Van Name <17864124+jvanname@users.noreply.github.com>
2022-11-15 15:07:26 -0800

    Update spectralradiusattack.jl

--- src/spectralradiusattack.jl
+++ src/spectralradiusattack.jl
@@ -3,20 +3,22 @@ include("aes.jl")
 GaloisFieldBasis=[UInt8(1),UInt8(2),UInt8(4),UInt8(8),UInt8(16),UInt8(32),UInt8(64),UInt8(128)];
 
 function vectobyte(x)
-output=[];
+output=zeros(Int64,8);
 for i in 1:8
 if x&GaloisFieldBasis[i]==0
       output[i]=0;
     else output[i]=1;
     end;
   end;
+return output;
 end;
 
 function bytetovec(x)
-b=0;
+b=UInt8(0);
             for i in 1:8
                   if x[i]==1
                       b+=GaloisFieldBasis[i];  
                   end;
 end;
+return b;
 end;
